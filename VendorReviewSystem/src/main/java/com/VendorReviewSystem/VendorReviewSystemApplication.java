ab6ec5a9beb272d779fbe0ac38cce6c3a457d3fb
GokulPrasand <spgokulprasand23@gmail.com>
2022-03-14 20:29:20 +0530

    Skeleton code

---
+++ VendorReviewSystem/src/main/java/com/VendorReviewSystem/VendorReviewSystemApplication.java
@@ -0,0 +1,14 @@
+package com.VendorReviewSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorReviewSystemApplication.class, args);
+		System.out.println("SERVICE STARTED!!!!");
+	}
+
+}
---
+++ VendorReviewSystem/src/test/java/com/VendorReviewSystem/VendorReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VendorReviewSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
